996a68be87985bff3c5916776bf2687d8eed775b
asurowy <101614024+asurowy@users.noreply.github.com>
2022-07-29 14:28:14 -0400

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob'
+// let bestCustomer;
+
+function upperCaseCustomerName(){
+      console.log(customerName.toUpperCase());
+        customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+//setBestCustomer();
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'some inital value'
+
+function changeLeastFavoriteCustomer(){
+        leastFavoriteCustomer = 'a different value'
+}
